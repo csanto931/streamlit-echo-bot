3d133507323210cbad2c21ada8246bda798f4904
Chris S. <8145582+csanto931@users.noreply.github.com>
2026-03-06 14:14:17 -0700

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -5,7 +5,7 @@ import requests
 import json
 
 def response_generator():
-    response = ai_ask("Pretend you are a very friendly and helpful person.  " +
+    response = ai_ask("Pretend like you are a witch doctor ready to treat a patient.  " +
                       "Please provide a response given the provided context.  " +
                       "Please provide the response only with no before or after commentary.",
                       data=st.session_state.messages,
